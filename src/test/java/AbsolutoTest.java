6f3104a7ed24eeec39d6ad7dd9235dd5def40818
saravana1955 <saravana.kumar@provility.com>
2024-08-26 12:15:37 +0530

    refactor code

--- src/test/java/AbsolutoTest.java
+++ src/test/java/AbsolutoTest.java
@@ -8,7 +8,7 @@ public class AbsolutoTest {
         //arrange
         Absoluto absoluto = new Absoluto();
         int actual;
-        int expect = 2;
+        int expect = 3;
 
         //act
         actual = absoluto.retornaValorAbsoluto(-2);
